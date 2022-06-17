db946b23c81b5a0c64e82d6f9533146b5c2d425d
aman-109 <aman.mate.52@gmail.com>
2022-06-17 22:06:34 +0530

    footer component added

---
+++ Hotel_Stay/components/footer.js
@@ -0,0 +1,61 @@
+
+
+function footer(){
+    return ` <div id="footer_child">
+    <!-- upper part -->
+    <div id="Up_footer">
+        <!-- logo div -->
+        <div class="F_contactDiv">
+            <img class="F_companylogo" src="https://a.travel-assets.com/globalcontrols-service/content/f285fb631b0a976202ef57611c7050e9ef5ca51a/images/EG_Wordmark_blue_RGB.svg" >
+        </div>
+        <div class="F_contactDiv">
+            <h3 class="F_tagName">Company</h3>
+            
+            <a href="">About us</a>
+            <a href="">Jobs</a>
+            <a href="">List your property</a>
+            <a href="">Partnerships</a>
+        </div>
+        <div class="F_contactDiv">
+            <h3 class="F_tagName">Explore</h3>
+            
+            <a href="">India travel guide</a>
+            <a href="">Hotels in india</a>
+            <a href="">Holiday rentals in india</a>
+            <a href="">Holiday packages in india</a>
+            <a href="">Domestic flights</a>
+            <a href="">Car hire in India</a>
+            <a href="">All accommodation types</a>
+            <a href="">Travel blog</a>
+        </div>
+        <div class="F_contactDiv">
+            <h3 class="F_tagName">Terms and policies</h3>
+            
+            <a href="">Privacy Statement</a>
+            <a href="">Terms of use</a>
+            <a href="">Virbo terms and conditions</a>
+        </div>
+        <div class="F_contactDiv">
+            <h3 class="F_tagName">Help</h3>
+            
+            <a href="">Support</a>
+            <a href="">Change or cancel your booking</a>
+            <a href="">Refund process and timelines</a>
+            <a href="">Book a flight using an airline credit</a>
+            <a href="">International travel document</a>
+        </div>
+    </div>
+
+    <!-- lower part -->
+    <div id="Down_footer">
+        <div class="F_imgdiv">
+            <img class="F_securityLogo" src="https://images.trvl-media.com/media/content/expind/images/sg/secure.png" >
+
+        </div>
+        <p class="F_copyrightText">© 2022 Expedia, Inc., an Expedia Group company / Expedia Asia Holdings Mauritius / BEX Travel Asia Pte. Ltd. All rights reserved. Expedia and the Airplane Logo are trademarks or registered trademarks of Expedia, Inc.</p>
+    </div>
+</div>`
+}
+
+
+export default footer
